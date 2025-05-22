7a85914cb1f52a2b8a25d6e10e76d9c980b7402f
Sneka Chandrasekaran <sneka202420@gmail.com>
2025-05-22 14:20:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+let count = 0;
+
+const countEl = document.getElementById("count");
+const increaseBtn = document.getElementById("increase");
+const decreaseBtn = document.getElementById("decrease");
+const resetBtn = document.getElementById("reset");
+
+increaseBtn.addEventListener("click", () => {
+  count++;
+  updateDisplay();
+});
+
+decreaseBtn.addEventListener("click", () => {
+  count--;
+  updateDisplay();
+});
+
+resetBtn.addEventListener("click", () => {
+  count = 0;
+  updateDisplay();
+});
+
+function updateDisplay() {
+  countEl.textContent = count;
+}
